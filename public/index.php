e72614b1e3e742a85559b31208cc151707f72e0d
Francisco Martins <francisco_jcm_7@hotmail.com>
2018-10-17 21:35:09 +0100

    MINOR: Core index.php changes.

--- public/index.php
+++ public/index.php
@@ -18,48 +18,27 @@ use app\services\session\SessionService;
 use app\services\TranslatorService;
 use app\helpers\SecurityHelper;
 
-/**
- * Ficheiro de Bootstrap do phalcon
- * Autoload
- * 
- * @see https://github.com/phalcon/docs/blob/3.3/en/tutorial-base.md/#bootstrap - Docs
- */
 $loader = new Loader();
-/* Registas o diretorios para a framework saber onde estao as coisas */
 $loader->registerDirs(
         [
             "../apps/controllers/",
             "../apps/models/",
             "../apps/helpers/"
         ]
-);
-/* Registo de namespaces */
-$loader->registerNamespaces(
+)->registerNamespaces(
         [
             "app\helper" => "../apps/helpers/",
             "app\services\session" => "../apps/services",
             "app\services" => "../apps/services"
         ]
-);
-/* inicia os parametros na framework */
-$loader->register();
+)->register();
 
 $di = new Di();
-
-// Registering a router
 $di->set("router", Router::class);
-
 $di->set("url", Url::class);
-
-// Registering a dispatcher
 $di->set("dispatcher", MvcDispatcher::class);
-
-// Registering a Http\Response
 $di->set("response", Response::class);
-
-// Registering a Http\Request
 $di->set("request", Request::class);
-
 $di->set(
         "voltService", function ($view, $di) {
     $volt = new Volt($view, $di);
@@ -68,14 +47,13 @@ $di->set(
             [
                 "compiledPath" => "../apps/cache/",
                 "compiledExtension" => ".compiled",
+                "compiledSeparator" => "_",
                 'compileAlways' => true
             ]
     );
 
     return $volt;
-}
-);
-// Registering the view component
+});
 $di->set(
         "view", function () {
     $view = new View();
@@ -116,23 +94,13 @@ $di->set(
         "tradutor", function () {
     return new TranslatorService();
 }, true);
-
-//Registering the Models-Metadata
 $di->set("modelsMetadata", ModelMetadata::class);
-
-//Registering the Models Manager
 $di->set("modelsManager", ModelManager::class);
+$di->set("crypt", function () { return new SecurityHelper(); });
 
-$di->set(
-        "crypt", function () {
-    return new SecurityHelper();
-}
-);
 try {
     $application = new Application($di);
-
     $response = $application->handle();
-
     echo $response->getContent();
 } catch (Exception $e) {
     echo $e->getMessage();
