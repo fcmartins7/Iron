6b498fe3e6c85676f793d6cf9d82904a81a723fe
Francisco José Costa Martins <francisco_jcm_7@hotmail.com>
2018-10-22 10:05:33 +0100

    BUGFIX: Fixed application loader. Now loads correctly from a configuration file.

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,9 +1,17 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+use \Phalcon\Config;
 
+$configuration = new Config([
+    'namespaces' => [
+        "app\helper" => "../app/helpers/",
+        "app\services\session" => "../app/services",
+        "app\services" => "../app/services",
+        "Iron\Services\Core" => '../app/services/core'],
+    'folders' => [
+        "../app/controllers/",
+        "../app/models/",
+        "../app/helpers/",
+    ]]
+);
 
--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 <?php
 
 error_log(E_ALL);
+define('APP_PATH', realpath('..') . '/');
 
 use Iron\Services\Core\Dependencys;
 use Phalcon\Mvc\Application;
 use Phalcon\Loader;
-
-use \Phalcon\Config;
 
 /**
  * Application dependency injector.
@@ -14,29 +13,19 @@ use \Phalcon\Config;
  * @author      Francisco Martins   <francisco_jcm_7@hotmail.com>
  * @version     000.000.001         @10/09/2018
  */
-$configuration = new Config([
-    'namespaces' => [
-        "app\helper" => "../app/helpers/",
-        "app\services\session" => "../app/services",
-        "app\services" => "../app/services",
-        "Iron\Services\Core" => '../app/services/core'],
-    'folders' => [
-        "../app/controllers/",
-        "../app/models/",
-        "../app/helpers/",
-    ]]
-);
+require_once APP_PATH . 'app/config/Config.php';
 
 try {
-    $appLoader = new Loader();
-
-    $appLoader->registerDirs(
-            $configuration->folders
+    $loader = new Loader();
+    $loader->registerDirs(
+            (array) $configuration->folders
     )->registerNamespaces(
-            $configuration->namespaces
+            (array) $configuration->namespaces
     )->register();
-    $application = new Application(new Dependencys());
-    echo $application->handle()->getContent();
+    
+    $app = new Application(new Dependencys());
+    
+    echo $app->handle()->getContent();
 } catch (Exception $e) {
     echo $e->getMessage();
 }
